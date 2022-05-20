784fce1ebc8a6f3e47332b0a9b48e133d3692783
Calvin Jary <36313644+calvinjary@users.noreply.github.com>
2022-05-19 22:51:02 -0400

    Update 1 Main - Classification.py

--- 1 Main - Classification.py
+++ 1 Main - Classification.py
@@ -7,15 +7,9 @@ Features:
     3. full support for training and testing loader, allowing chunks to be loaded and tested
     4. support for any number of features, as well as any prediction window size, and any sequence length
 
-
-
-lecture 51
-6 minutes in
-
-
 Comments about Variable names:
     "Train" means the data used for training (the ground truth, the data known and used to train the model). "Test" means the unknown data not used to train the model that we used to test the model's accuracy
-    "X" implies the input data (meaning the feature information, the input data to the model) and "Y" implies the label (the ground truth, the value to the predicted, the output of the model)
+    "x" implies the input data (meaning the feature information, the input data to the model) and "Y" implies the label (the ground truth, the value to the predicted, the output of the model)
     
 In the context of the inner workings of the RNN/GRU/LSTM:
     X = input
